658cd5bda8e3d903b10ca857351bef797bc20112
Naman Jain <jnaman475@hmail.com>
2022-03-28 21:46:44 +0530

    minions version ready

--- app.js
+++ app.js
@@ -9,11 +9,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL (text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something went wrong ! please try later")
+}
+
 
 function clickHandler() {
-outputDiv.innerText = "ckhnsdkjvnj", + txtInput.value
+// outputDiv.innerText = "ckhnsdkjvnj " + txtInput.value
 
+var inputText = txtInput.value; 
+
+// calling the server
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then (json=> {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+})
+    .catch(errorHandler)
 };
 
 btnTranslate .addEventListener("click", clickHandler)
-  
+   
